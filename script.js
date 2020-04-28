9d653e4f6305416fb52d8c79866a7897a73dab45
danieldcaesar <danieldcaesar@gmail.com>
2020-04-27 23:23:41 -0400

    added var for speechrecognition object added var for additional paragraph

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 // caters for Firefox & Chrome browsers
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
 
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+
+let para = document.createElement('p');
+const words = document.querySelector('.words');
+words.appendChild(para);
+
