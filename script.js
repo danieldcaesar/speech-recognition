80cc57492c5d25924a26f4d2662f2e15fc70412a
danieldcaesar <danieldcaesar@gmail.com>
2020-04-28 09:39:23 -0400

    added event listener
    text is supposed to populate the page...?

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ let para = document.createElement('p');
 const words = document.querySelector('.words');
 words.appendChild(para);
 
+recognition.addEventListener('result', evt => {
+    const transcript = Array.from(evt.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('');
+    
+        p.textContent = transcript;
+    console.log(transcript);
+});
+
+recognition.start();
+recognition.addEventListener('end', recognition.start);
